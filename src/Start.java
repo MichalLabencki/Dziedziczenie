cc9f035fc4d4fd6673cfa56886f53015d109840f
MichalLabencki <74935701+MichalLabencki@users.noreply.github.com>
2021-01-15 15:19:09 +0100

    Initial commit

---
+++ src/Start.java
@@ -0,0 +1,19 @@
+
+class Start {
+    public static void main(String[] args)
+        {
+
+       System.out.println("Hello Michal");
+
+       int myFirstNumber = (10 + 5) + (10 * 2);
+       int mySecondNumber = 12;
+       int myThirdNumber = myFirstNumber * 2;
+       int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+       System.out.println(myTotal);
+       System.out.println(myFirstNumber);
+       int myLastOne = 1000 - myTotal;
+            System.out.println(myLastOne);
+
+    }
+
+}
